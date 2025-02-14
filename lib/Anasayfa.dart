61875312ba6dd3fec32275e0c7493b95bb018d4c
Mehmethan YUMUS <mehmethanyumus@Mehmethans-MacBook-Air.local>
2025-02-14 01:51:48 +0000

    Appbar tasarimi yapildi ve ulke arama fonksiyonu eklendi.

--- lib/Anasayfa.dart
+++ lib/Anasayfa.dart
@@ -16,6 +16,10 @@ class _AnasayfaState extends State<Anasayfa> {
       "maps,population,continents";
 
   final List<Ulke> _ulkeler = [];
+  bool aramaAktif = false;
+  TextEditingController _controllerArama = TextEditingController();
+  FocusNode aramaFocus = FocusNode();
+  List<Ulke> arananUlkeler = [];
 
   @override
   void initState() {
@@ -36,18 +40,79 @@ class _AnasayfaState extends State<Anasayfa> {
   }
 
   Widget _buildBody(){
+    /*
+      Ana body tasarimi
+     */
     return Scaffold(
+      appBar: _buildAppBar(),
       floatingActionButton: _buildFloatingButton(),
       body: Column(
         children: [
           Expanded(
             child: ListView.builder(
-              itemCount: _ulkeler.length,
+              itemCount: aramaAktif ? arananUlkeler.length : _ulkeler.length,
               itemBuilder: _buildListViewBuilder,
             ),
           )
         ],
       ),
+    );
+  }
+
+  AppBar _buildAppBar(){
+    /*
+      App bar tasarimi
+      * Burada arama durumuna bagli olarak tasarim yapildi.
+      * Arama aktifse, textfield eklemesi yapildi.
+     */
+    return AppBar(
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          if(!aramaAktif)
+            Text("Ülkeler", style: TextStyle(color: Colors.white),)
+          else
+            Expanded(
+              child: TextField(
+                controller: _controllerArama,
+                focusNode: aramaFocus,
+                decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)
+                  )
+                ),
+                cursorColor: Colors.white,
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                onChanged: _aramaYap,
+              ),
+            ),
+          IconButton(
+            onPressed: (){
+              setState(() {
+                aramaAktif = !aramaAktif;
+                if(aramaAktif){
+                  // arama true olunca klavyeyi açalım
+                  Future.delayed(Duration(milliseconds: 100), (){
+                    aramaFocus.requestFocus();
+                  });
+                }else{
+                  // arama false durumuna geçince değerleri temizleyelim
+                  arananUlkeler.clear();
+                  _controllerArama.clear();
+                  aramaFocus.unfocus();
+                }
+              });
+            },
+            icon: aramaAktif
+                ? Icon(Icons.cancel_outlined, color: Colors.white, size: 30,)
+                : Icon(Icons.search, color: Colors.white, size: 30,),
+          )
+        ],
+      ),
+      backgroundColor: Color(0xFF111DAB),
     );
   }
 
@@ -67,20 +132,43 @@ class _AnasayfaState extends State<Anasayfa> {
   }
 
   Widget _buildListViewBuilder(BuildContext context, int index){
+    /*
+      Eger arama true durumundaysa, kullanıcıya text girişine göre
+      ülkeleri gösterelim.
+     */
+    List<Ulke> gosterilecekUlkeler = aramaAktif ? arananUlkeler : _ulkeler;
+
     return Card(
       child: ListTile(
-        title: Text(_ulkeler[index].ulke_ad),
-        subtitle: Text("Başkent: ${_ulkeler[index].ulke_baskent}"),
-        leading: CircleAvatar(backgroundImage: NetworkImage(_ulkeler[index].ulke_bayrak),),
+        title: Text(gosterilecekUlkeler[index].ulke_ad),
+        subtitle: Text("Başkent: ${gosterilecekUlkeler[index].ulke_baskent}"),
+        leading: CircleAvatar(backgroundImage: NetworkImage(gosterilecekUlkeler[index].ulke_bayrak),),
         trailing: Icon(Icons.favorite_border, color: Colors.red,),
       ),
     );
   }
 
+  void _aramaYap(String aramaGiris){
+    /*
+      Kullanıcının her girdiği harfe karşılık olarak arama yapalım
+     */
+    setState(() {
+      String arananKelime = aramaGiris.trim().toLowerCase();
+      if(arananKelime.isNotEmpty){
+        arananUlkeler = _ulkeler
+            .where((ulke) => ulke.ulke_ad.toLowerCase().contains(arananKelime))
+            .toList();
+      }
+    });
+  }
   void _getUlkeler() async {
+    /*
+      API kullanarak ulke bilgilerini alalim
+     */
     Uri uri = Uri.parse(_apiURL);
     Response response = await get(uri);
 
+    // Gelen JSON response Liste türünde gelmekte
     List<dynamic> parsedResponse = jsonDecode(response.body);
 
     for(int i = 0; i < parsedResponse.length; i++){
@@ -89,6 +177,7 @@ class _AnasayfaState extends State<Anasayfa> {
 
       _ulkeler.add(ulke);
     }
+    // Ulke adına göre sıralama yapalım
     _ulkeler.sort((a, b) => a.ulke_ad.compareTo(b.ulke_ad));
     setState(() {});
   }
